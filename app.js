8164cc7053384d7aa1485c8d151851ea455cee36
innerstella <suji0509@naver.com>
2022-01-16 19:21:33 +0900

    링크 전까지 완성 (건들지 않기)

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
 const searchInput = document.querySelector("#search-form input");
 const searchForm = document.querySelector("#search-form");
-const searchArea = document.querySelector('.searchArea');
+const searchArea = document.querySelector('#searchArea');
 
 const HIDDEN_CLASSNAME = "hidden";
-const LOCATION_KEY = "currentStorage";
-//const SHOWING = "showing";
+
 
 function onSearchSubmit(event) {
 	event.preventDefault();
 	searchForm.classList.add(HIDDEN_CLASSNAME);
 	const locationname = searchInput.value;
-	//localStorage.setItem("locationname", locationname);
-	localStorage.setItem(LOCATION_KEY, locationname);
-
-  if (locationname === ""){
-	  alert("장소를 입력하세요.");
-  }
-  console.log(locationname);
+	localStorage.setItem("locationname", locationname);
+	console.log(locationname);
 }
 
 searchForm.addEventListener("submit", onSearchSubmit);

9c5aa52487d38d7f1413e16badce6e350cacc4c0
innerstella <suji0509@naver.com>
2022-01-27 16:32:46 +0900

    사소한 수정

--- js/main.js
+++ js/main.js
@@ -7,25 +7,17 @@ const HIDDEN_CLASSNAME = "hidden";
 window.onload = showImage;
 
 var backgroundImage = [
-	'./random_image/강승윤.jpg',
-	'./random_image/송민호.jpg',
-	'./random_image/아낙네.jpg',
-	'./random_image/위너.jpg',
-	'./random_image/이장준.jpg',
-	'./random_image/임창균.jpg',
-	'./random_image/홍중.jpg',
-	'./random_image/홍지수.jpg',
-	'./random_image/휘영.jpg',
-	'./random_image/원우.jpg',
-	'./random_image/기범.jpg',
-	'./random_image/윤현석.jpg',
-	'./random_image/조슈아.jpg',
-	'./random_image/최승철.jpg',
-	'./random_image/도겸.jpg',
-	'./random_image/용복.jpg',
-	'./random_image/호싱이.jpg',
-	'./random_image/채.jpg',
-	'./random_image/이준.jpg'
+	'./img_key/1.jpg',
+	'./img_key/2.jpg',
+	'./img_key/3.jpg',
+	'./img_key/4.jpg',
+	'./img_key/5.jpg',
+	'./img_key/6.jpg',
+	'./img_key/7.jpg',
+	'./img_key/8.jpg',
+	'./img_key/9.jpg',
+	'./img_key/10.jpg'
+
 ];
 
 function showImage(){
